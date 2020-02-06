a45caa83fa8328b9b0e528441fe85051e25778d5
Elena Choo <e0035848@u.nus.edu>
2020-02-06 22:13:09 +0800

    Add my first game

---
+++ Assets/Script/PlayerControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+	public float speed;
+	private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	// initialize a component of game object, type is Rigitbody
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        // add a force vector to a Rigidbody
+        rb.AddForce(movement * speed);
+    }
+
+    // default function to detect object collisions
+    void OnTriggerEnter(Collider other) {
+        // check if object entering is 'pick up'
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            // set object as invisible once collide
+            other.gameObject.SetActive(false);
+        }
+    }
+}
